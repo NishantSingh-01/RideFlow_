5154d11947e86c60edbf4137ff579671856f98a3
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-05-29 22:20:14 +0530

    added user service

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv'
 import app from '../src/app.js'
+import connectdb from './db/db.js'
 dotenv.config()
 
-
+connectdb()
 
 app.listen(process.env.PORT,()=>{
     console.log(`The Server is Running ${process.env.PORT}`)
