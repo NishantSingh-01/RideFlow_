2438adc64f583a8e389396fa08eb4e7aab4193a5
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-05-30 22:58:47 +0530

    added user repositories folder

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,14 +3,6 @@ const { Pool } = pkg
 import dotenv from "dotenv"
 dotenv.config()
 
-console.log({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: String(process.env.DB_PASSWORD),
-  port: Number(process.env.DB_PORT),
-})
-
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
@@ -18,7 +10,6 @@ const pool = new Pool({
     password: String(process.env.DB_PASSWORD),
     port: Number(process.env.DB_PORT)
 })
-
 
 // pool.on("connect", () => {
 //     console.log("Connection pool established with Database")
---
+++ server/src/repositories/user.repositories.js
@@ -0,0 +1,26 @@
+import { pool } from "../config/db.js"
+
+
+const createUser = async (firstname, lastname, email, password) => {
+    const result = await pool.query(
+        `
+        INSERT INTO users(firstname,lastname,email,password)
+         VALUES($1,$2,$3 ,$4)
+         RETURNING *
+         `
+        , [firstname, lastname, email, password])
+
+    return result.rows[0]
+}
+const findUserByEmail = async (email) => {
+    const result = await pool.query(
+        `
+        SELECT *
+        FROM users
+        WHERE email = $1
+    `,
+        [email]
+    )
+    return result.rows[0];
+}
+export { createUser, findUserByEmail }
