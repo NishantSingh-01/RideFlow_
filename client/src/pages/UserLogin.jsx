6428f59d91ba15e34f8b50ee79ecfe6ae96ccd65
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-06-04 19:07:58 +0530

    added login user

--- client/src/pages/UserLogin.jsx
+++ client/src/pages/UserLogin.jsx
@@ -2,8 +2,49 @@ import React from 'react'
 
 const UserLogin = () => {
   return (
-    <div>
-      ul
+    <div className="h-screen w-screen flex justify-center ">
+
+      <form className=" p-8 rounded-lg shadow-md flex flex-col gap-4 w-100">
+        <div className='font-mono text-lg font-bold'>RideFlow</div>
+        <label htmlFor="email" className="font-medium">
+          Enter your Email
+        </label>
+
+        <input
+          id="email"
+          type="email"
+          placeholder="example@gmail.com"
+          className="p-3 border rounded-md outline-none focus:border-blue-500"
+        />
+
+        <label htmlFor="password" className="font-medium">
+          Enter your Password
+        </label>
+
+        <input
+          id="password"
+          type="password"
+          placeholder="Your Password"
+          className="p-3 border rounded-md outline-none focus:border-blue-500"
+        />
+
+        <button
+          type="submit"
+          className="bg-black mt-4 text-white p-3 rounded-md hover:bg-gray-900"
+        >
+          Login
+        </button>
+        <div className="mt-auto mb-2">
+          <button
+            type="button"
+            className="w-full bg-green-600 text-white p-3 rounded-md"
+          >
+            Login as Captain
+          </button>
+        </div>
+
+
+      </form>
     </div>
   )
 }
