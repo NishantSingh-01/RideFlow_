1e14ed48d2955f94d45ae33089b36412840c9eb1
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-06-05 14:10:32 +0530

    added loader

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="h-screen w-screen bg-white flex items-center justify-center">
+      <div className="w-12 h-12 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+    </div>
+  )
+}
+
+export default Loader
