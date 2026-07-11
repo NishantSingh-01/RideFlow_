31083b1b6124fdb5764084689588cfcc66bc9d04
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-07-12 00:21:26 +0530

    added request ride done

--- client/src/pages/RequestRide.jsx
+++ client/src/pages/RequestRide.jsx
@@ -32,8 +32,8 @@ const ConfirmRide = () => {
             <NormalNav />
             <div className='flex flex-col h-full w-full gap-4  md:flex-row-reverse md:justify-around md:gap-10'>
                 <div className='flex md:mt-26 mt-7 flex-col items- justify-center  md:flex-row-reverse md:justify-around md:gap-14'>
-                    <div className='h-[50vh] w-full   overflow-x-hidden md:h-[490px] md:w-[800px] shadow-xl rounded-lg overflow-hidden relative z-0'>
-                        <Map />
+                    <div className='h-[50vh] w-full   overflow-x-hidden md:h-[470px] md:w-[800px] shadow-xl rounded-lg overflow-hidden relative z-0'>
+                        <Map /> //TODO get routes 
                     </div>
                     <div className='  w-full h-[39vh] rounded-xl md:w-[500px]  p-4 '>
                         {confirmedRide ? (
@@ -46,17 +46,32 @@ const ConfirmRide = () => {
                             //     <p className="text-lg">Fare: <span className="font-semibold">${confirmedRide.fare}</span></p>
                             //     <p className="text-lg">Please wait for your captain to arrive.</p>
                             // </div>
-                            <div>
-                                <h1 className='text-center text-2xl font-mono font-bold'>Ride Accepted</h1>
-                                <div className='flex justify-around items-center gap-9 mt-4'>
+                            <div className='flex justify-center flex-col pb-2 mt-3'>
+                                <h1 className='text-center text-2xl font-mono font-bold md:text-3xl '>Ride Accepted by Captain ...</h1>
+                                <div className='flex justify-around items-center gap-3 mt-2 md:mt-5'>
                                     <div className='h-17 w-17 rounded-full border-2 border-cyan-700 overflow-hidden'>
                                         <img className='h-full w-full object-cover' src="https://png.pngtree.com/png-vector/20191021/ourmid/pngtree-vector-driver-icon-png-image_1834504.jpg" alt="" />
                                     </div>
                                     <div>
-                                        <p className="font-semibold text-xl">{confirmedRide.captain_firstname}  {confirmedRide.captain_lastname}</p>
-                                        <p></p>
+                                        <p className="font-semibold text-xl">{confirmedRide.captain_firstname} {confirmedRide.captain_lastname}</p>
+                                        {/* <p>{confirmedRide.vehicle_color} Vehicle</p> */}
+                                        <p className='font-sans font-semibold text-blue-800'>{confirmedRide.vehicle_plate}</p>
+                                    </div>
+                                    
+                                </div>
+                                <div className='flex items-center justify-center gap-8 font-semibold text-lg mt-6 p-3 bg-gradient-to-r from-blue-50 to-cyan-50 rounded-lg'>
+                                    <div className='flex items-center gap-2'>
+                                        <span className='text-blue-600 font-bold'>{confirmedRide.distance ? `${(Number(confirmedRide.distance) / 1000).toFixed(1)}` : '0'}</span>
+                                        <span className='text-gray-700'>km away</span>
+                                    </div>
+                                    <div className='h-6 w-[3px] rounded bg-gray-500'></div>
+                                    <div className='flex items-center gap-2'>
+                                        <span className='text-blue-600 font-bold'>{confirmedRide.duration ? `${Math.round(Number(confirmedRide.duration) / 60)}` : '0'}</span>
+                                        <span className='text-gray-700'>mins</span>
                                     </div>
                                 </div>
+                                <p className='text-center text-lg font-semibold text-gray-700 mt-4 md:mt-6'>Your Captain is on the Way</p>
+                                <button className='p-2.5 border-3 border-red-600 text-red-600 font-semibold rounded-lg w-full mt-4 transition-colors md:mt-6'>Cancel Ride</button>
 
                             </div>
                         ) : (
